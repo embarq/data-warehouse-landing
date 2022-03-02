4aa5004855740b7c3d43c7a6f9553034d63664ee
embarq <embarq.dev@gmail.com>
2022-03-02 20:46:48 +0200

    add fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,27 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+/** @type {typeof defaultTheme} */
 module.exports = {
   content: [
     "./src/**/*.{html,js}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+      },
+    },
+    fontFamily: {
+      heading: [
+        'Helvetica',
+        ...defaultTheme.fontFamily.sans
+      ],
+      sans: [
+        'Avenir LT Std',
+        ...defaultTheme.fontFamily.sans
+      ]
+    },
   },
   plugins: [],
 }
